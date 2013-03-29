c1f3a8f8780a30e0b99a18ad91c03af6534b53f8
tomc <>
2013-03-29 10:32:35 +0000

    Add runC3

--- Crab/makeCrab.py
+++ Crab/makeCrab.py
@@ -8,18 +8,20 @@ samples = {
 	   # Single muon
 	   'SingleMuonA' : 	['data',	'/SingleMu/Run2012A-13Jul2012-v1/AOD', 			'Cert_190456-196531_8TeV_13Jul2012ReReco_Collisions12_JSON_v2.txt'],
 	   'SingleMuonA2' : 	['data',	'/SingleMu/Run2012A-recover-06Aug2012-v1/AOD', 		'Cert_190782-190949_8TeV_06Aug2012ReReco_Collisions12_JSON.txt'],
-	   'SingleMuonB' : 	['data',	'/SingleMu/Run2012B-13Jul2012-v4/AOD', 			'Cert_190456-196531_8TeV_13Jul2012ReReco_Collisions12_JSON_v2.txt'],
+	   'SingleMuonB' : 	['data',	'/SingleMu/Run2012B-13Jul2012-v1/AOD', 			'Cert_190456-196531_8TeV_13Jul2012ReReco_Collisions12_JSON_v2.txt'],
 	   'SingleMuonC' : 	['data',	'/SingleMu/Run2012C-24Aug2012-v1/AOD', 			'Cert_198022-198523_8TeV_24Aug2012ReReco_Collisions12_JSON.txt'],
-	   'SingleMuonC2' : 	['data',	'',							'Cert_190456-203002_8TeV_PromptReco_Collisions12_JSON_v2.txt'],
+	   'SingleMuonC2' : 	['data',	'/SingleMu/Run2012C-PromptReco-v2/AOD',			'Cert_190456-203002_8TeV_PromptReco_Collisions12_JSON_v2.txt'],
+	   'SingleMuonC3' : 	['data',	'/SingleMu/Run2012C-EcalRecover_11Dec2012-v1/AOD',	'Cert_201191-201191_8TeV_11Dec2012ReReco-recover_Collisions12_JSON.txt'],
 	   'SingleMuonD' : 	['data',	'/SingleMu/Run2012D-PromptReco-v1/AOD', 		'Cert_190456-208686_8TeV_PromptReco_Collisions12_JSON_PixAlignRecovery.txt'],
 	   'SingleMuonD2' : 	['data',	'', 							'Cert_207883-208307_8TeV_16Jan2013ReReco_Collisions12_JSON.txt'],
 
 	   # Single electron
            'SingleElectronA' : 	['data',	'/SingleElectron/Run2012A-13Jul2012-v1/AOD', 		'Cert_190456-196531_8TeV_13Jul2012ReReco_Collisions12_JSON_v2.txt'],
 	   'SingleElectronA2' :	['data',	'/SingleElectron/Run2012A-recover-06Aug2012-v1/AOD', 	'Cert_190782-190949_8TeV_06Aug2012ReReco_Collisions12_JSON.txt'],
-	   'SingleElectronB' : 	['data',	'/SingleElectron/Run2012B-13Jul2012-v4/AOD', 		'Cert_190456-196531_8TeV_13Jul2012ReReco_Collisions12_JSON_v2.txt'],
+	   'SingleElectronB' : 	['data',	'/SingleElectron/Run2012B-13Jul2012-v1/AOD', 		'Cert_190456-196531_8TeV_13Jul2012ReReco_Collisions12_JSON_v2.txt'],
 	   'SingleElectronC' : 	['data',	'/SingleElectron/Run2012C-24Aug2012-v1/AOD', 		'Cert_198022-198523_8TeV_24Aug2012ReReco_Collisions12_JSON.txt'],
-	   'SingleElectronC2' :	['data',	'', 							'Cert_190456-203002_8TeV_PromptReco_Collisions12_JSON_v2.txt'],
+	   'SingleElectronC2' :	['data',	'/SingleElectron/Run2012C-PromptReco-v2/AOD', 		'Cert_190456-203002_8TeV_PromptReco_Collisions12_JSON_v2.txt'],
+	   'SingleElectronC3' :	['data',	'/SingleElectron/Run2012C-EcalRecover_11Dec2012-v1/AOD','Cert_201191-201191_8TeV_11Dec2012ReReco-recover_Collisions12_JSON.txt'],
 	   'SingleElectronD' : 	['data',	'/SingleElectron/Run2012D-PromptReco-v1/AOD', 		'Cert_190456-208686_8TeV_PromptReco_Collisions12_JSON_PixAlignRecovery.txt'],
 	   'SingleElectronD2' :	['data',	'', 							'Cert_207883-208307_8TeV_16Jan2013ReReco_Collisions12_JSON.txt'],
 
@@ -28,18 +30,20 @@ samples = {
 	   'DoubleMuonA2' : 	['data',	'/DoubleMu/Run2012A-recover-06Aug2012-v1/AOD', 		'Cert_190782-190949_8TeV_06Aug2012ReReco_Collisions12_JSON.txt'],
 	   'DoubleMuonB' : 	['data',	'/DoubleMu/Run2012B-13Jul2012-v4/AOD', 			'Cert_190456-196531_8TeV_13Jul2012ReReco_Collisions12_JSON_v2.txt'],
 	   'DoubleMuonC' : 	['data',	'/DoubleMu/Run2012C-24Aug2012-v1/AOD', 			'Cert_198022-198523_8TeV_24Aug2012ReReco_Collisions12_JSON.txt'],
-	   'DoubleMuonC2' : 	['data',	'', 							'Cert_190456-203002_8TeV_PromptReco_Collisions12_JSON_v2.txt'],
+	   'DoubleMuonC2' : 	['data',	'/DoubleMu/Run2012C-PromptReco-v2/AOD', 		'Cert_190456-203002_8TeV_PromptReco_Collisions12_JSON_v2.txt'],
+	   'DoubleMuonC3' : 	['data',	'/DoubleMu/Run2012C-EcalRecover_11Dec2012-v1/AOD',	'Cert_201191-201191_8TeV_11Dec2012ReReco-recover_Collisions12_JSON.txt'],
 	   'DoubleMuonD' : 	['data',	'/DoubleMu/Run2012D-PromptReco-v1/AOD', 		'Cert_190456-208686_8TeV_PromptReco_Collisions12_JSON_PixAlignRecovery.txt'],
 	   'DoubleMuonD2' : 	['data',	'/DoubleMu/Run2012D-16Jan2013-v2/AOD', 			'Cert_207883-208307_8TeV_16Jan2013ReReco_Collisions12_JSON.txt'],
 
 	   # Double electron
            'DoubleElectronA' : 	['data',	'/DoubleElectron/Run2012A-13Jul2012-v1/AOD', 		'Cert_190456-196531_8TeV_13Jul2012ReReco_Collisions12_JSON_v2.txt'],
 	   'DoubleElectronA2' :	['data',	'/DoubleElectron/Run2012A-recover-06Aug2012-v1/AOD', 	'Cert_190782-190949_8TeV_06Aug2012ReReco_Collisions12_JSON.txt'],
-	   'DoubleElectronB' : 	['data',	'/DoubleElectron/Run2012B-13Jul2012-v4/AOD', 		'Cert_190456-196531_8TeV_13Jul2012ReReco_Collisions12_JSON_v2.txt'],
+	   'DoubleElectronB' : 	['data',	'/DoubleElectron/Run2012B-13Jul2012-v1/AOD', 		'Cert_190456-196531_8TeV_13Jul2012ReReco_Collisions12_JSON_v2.txt'],
 	   'DoubleElectronC' : 	['data',	'/DoubleElectron/Run2012C-24Aug2012-v1/AOD', 		'Cert_198022-198523_8TeV_24Aug2012ReReco_Collisions12_JSON.txt'],
-	   'DoubleElectronC2' :	['data',	'', 							'Cert_190456-203002_8TeV_PromptReco_Collisions12_JSON_v2.txt'],
+	   'DoubleElectronC2' :	['data',	'/DoubleElectron/Run2012C-PromptReco-v1/AOD', 		'Cert_190456-203002_8TeV_PromptReco_Collisions12_JSON_v2.txt'],
+	   'DoubleElectronC3' : ['data',	'/DoubleElectron/Run2012C-EcalRecover_11Dec2012-v1/AOD','Cert_201191-201191_8TeV_11Dec2012ReReco-recover_Collisions12_JSON.txt'],
 	   'DoubleElectronD' : 	['data',	'/DoubleElectron/Run2012D-PromptReco-v1/AOD', 		'Cert_190456-208686_8TeV_PromptReco_Collisions12_JSON_PixAlignRecovery.txt'],
-	   'DoubleElectronD2' :	['data',	'/DoubleElectron/Run2012D-16Jan2013-v2/AOD', 		'Cert_207883-208307_8TeV_16Jan2013ReReco_Collisions12_JSON.txt'],
+	   'DoubleElectronD2' :	['data',	'/DoubleElectron/Run2012D-16Jan2013-v1/AOD', 		'Cert_207883-208307_8TeV_16Jan2013ReReco_Collisions12_JSON.txt'],
 
 	   # Signal
 	   'signal' :		['MC',		'/DYJJ01JetsToLL_M-50_MJJ-200_TuneZ2Star_8TeV-madgraph_tauola/Summer12_DR53X-PU_S10_START53_V7A-v1/AODSIM'],
