66f57d1eb1e1e9669de866bd801199c6bc33318b
UAEDF-tomc <tom.cornelis@ua.ac.be>
2013-09-11 10:37:09 +0200

    TMVA: update training trees tag updated

--- Macros/TMVAtraining/classification.C
+++ Macros/TMVAtraining/classification.C
@@ -18,7 +18,7 @@
 #include <TMVA/Config.h>
 #include "../environment.h"
 
-TString tag = "20130822_InclusiveForTMVATrees";
+TString tag = "20130910_InclusiveForTMVA";
 TString option = "_BDT_50k";
 TString DYtype = "inclusive";
    
