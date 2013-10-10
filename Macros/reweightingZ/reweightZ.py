9b110a302d8688b903485d360689d67ea179bfab
UAEDF-tomc <tom.cornelis@ua.ac.be>
2013-10-10 17:55:18 +0200

    Update for the Z reweighting module, including new pt weights

--- Macros/reweightingZ/reweightZ.py
+++ Macros/reweightingZ/reweightZ.py
@@ -15,17 +15,18 @@ def merge(sourceFile, plot, histList):
 
 for type in ["ZMUMU","ZEE"]:
   sourceFile = TFile(getTreeLocation() + "outputs/rootfiles/" + type + "/" + tag + ".root")
-  data = sourceFile.Get("dilepton_eta_data").Clone()
-  data.Add(merge(sourceFile, "dilepton_eta", ["TTJetsSemiLept","TTJetsFullLept","TTJetsHadronic","T-W","Tbar-W","T-s","Tbar-s","T-t","Tbar-t","WW","WZ","ZZ","WJets","ZVBF"]), -1)
-  DY = merge(sourceFile, "dilepton_eta", ["DY0","DY1","DY2","DY3","DY4"])
-  sumData = data.Integral()
-  sumDY = DY.Integral()
+  for plot in ["eta","pt"]:
+    data = sourceFile.Get("dilepton_" + plot + "_data").Clone()
+    data.Add(merge(sourceFile, "dilepton_" + plot, ["TTJetsSemiLept","TTJetsFullLept","TTJetsHadronic","T-W","Tbar-W","T-s","Tbar-s","T-t","Tbar-t","WW","WZ","ZZ","WJets","ZVBF"]), -1)
+    DY = merge(sourceFile, "dilepton_" + plot, ["DY0","DY1","DY2","DY3","DY4"])
+    sumData = data.Integral()
+    sumDY = DY.Integral()
 
-  f = open('rapidityWeigths_' + type + '.txt','w')
-  f.write('min\tmax\tweight\n')
-  for i in range(1, data.GetNbinsX() + 1):
-    ratio = data.GetBinContent(i)/DY.GetBinContent(i)*sumDY/sumData
-    f.write(str(data.GetBinLowEdge(i)) + '\t' + str(data.GetBinLowEdge(i+1)) + '\t' + str(ratio) + '\n')
-  f.close()
+    f = open(plot + 'Weigths_' + type + '.txt','w')
+    f.write('min\tmax\tweight\n')
+    for i in range(1, data.GetNbinsX() + 1):
+      ratio = (data.GetBinContent(i)/sumData)/(DY.GetBinContent(i)/sumDY)
+      f.write(str(data.GetBinLowEdge(i)) + '\t' + str(data.GetBinLowEdge(i+1)) + '\t' + str(ratio) + '\n')
+    f.close()
 
 exit()
