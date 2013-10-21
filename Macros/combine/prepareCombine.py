13679c4f90f5fc824e9a55503bd85ec69ae3c94e
UAEDF-tomc <tom.cornelis@ua.ac.be>
2013-10-21 16:26:27 +0200

    Takes more systematics, using the new file structure

--- Macros/combine/prepareCombine.py
+++ Macros/combine/prepareCombine.py
@@ -2,9 +2,7 @@
 from ROOT import TFile, TH1
  
 base = "BDT"
-tag = "20131015_Fast3"
-puUpTag = "20131017_66785_Fast"
-puDownTag = "20131017_73815_Fast"
+tag = "20131021_Fast"
 
 mcGroups = {'DY': ["DY0","DY1","DY2","DY3","DY4"],
             'TTJets': ["TTJetsSemiLept","TTJetsFullLept","TTJetsHadronic"],
@@ -15,38 +13,35 @@ mcGroups = {'DY': ["DY0","DY1","DY2","DY3","DY4"],
 def getTreeLocation():
   return "/user/tomc/public/merged/EWKV/2013-06-JetIDfix/"
 
-def merge(sourceFile, plot, histList):
-  histMerged = sourceFile.Get(plot + "_" + histList[0]).Clone()
-  for hist in histList[1:]: 
-    if sourceFile.FindKey(plot + "_" + hist): histMerged.Add(sourceFile.Get(plot + "_" + hist))
-  return histMerged
+def getPlot(sourceFile, sample, plot, ignoreNonExist = False):
+  if sourceFile.FindKey(sample + "/" + plot): return sourceFile.Get(sample + "/" + plot).Clone()
+  if sourceFile.FindKey(plot + "_" + sample): return sourceFile.Get(plot + "_" + sample).Clone()
+  if not ignoreNonExist: 
+    print sample + "/" + plot + " not found!"
+    exit(1)
 
+def merge(sourceFile, plot, histList):
+  histMerged = getPlot(sourceFile, histList[0], plot)
+  for hist in histList[1:]: histMerged.Add(getPlot(sourceFile, hist, plot, True))
+  return histMerged
 
 combineFile = TFile("ewkZjj_8TeV.root","RECREATE")
 for type in ["ZMUMU","ZEE"]: 
   print "prepare " + type
   sourceFile = TFile(getTreeLocation() + "outputs/rootfiles/" + type + "/" + tag + ".root")
-  puUpFile = TFile(getTreeLocation() + "outputs/rootfiles/" + type + "/" + puUpTag + ".root")
-  puDownFile = TFile(getTreeLocation() + "outputs/rootfiles/" + type + "/" + puDownTag + ".root")
   for category in ["","_100","_200"]:
     dir = combineFile.mkdir(type + category)
     dir.cd()
 
-    data = sourceFile.Get(base + category + "_data")
-    data.Write("data_obs")
+    getPlot(sourceFile, "data", base + category).Write("data_obs")
 
-    for systematic in ["","_JESUp","_JESDown","_PUUp","_PUDown"]:
-      file = sourceFile
-      if systematic == "_PUUp": file = puUpFile
-      if systematic == "_PUDown": file = puDownFile
-
-      plot = base + category
-      if systematic == "_JESUp": plot += "JES+"
-      if systematic == "_JESDown": plot += "JES-"
+    for systematic in ["","JESUp","JESDown","JERUp","JERDown","PUUp","PUDown","QGUp","QGDown","mcfmUp","mcfmDown"]:
+      plot = base + category + systematic
+      if systematic == "QGDown" or systematic == "mcfmDown": plot = base + category
 
       expected = {}
       for name, mcs in mcGroups.iteritems():
-        thisGroup = merge(file, plot, mcs)
+        thisGroup = merge(sourceFile, plot, mcs)
         thisGroup.Write(name + systematic)
         expected[name] = ('%.3f' % thisGroup.Integral())
        
