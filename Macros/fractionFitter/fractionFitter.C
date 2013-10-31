a95305339a16db8aea8cd875dafac18b812ee88a
UAEDF-tomc <tom.cornelis@ua.ac.be>
2013-10-31 15:33:05 +0100

    Take tag as command line option and output to file, change EWK lumi

--- Macros/fractionFitter/fractionFitter.C
+++ Macros/fractionFitter/fractionFitter.C
@@ -32,21 +32,28 @@
 using namespace std;
 
 TString plot = "BDT";
-TString tag = "20131021_Fast";
 
-int main(){
+int main(int argc, char *argv[]){
+  TString tag = ((TString) argv[1]);
+  std::vector<TString> types {"ZEE","ZMUMU"};								//If no type given as option, run both
+  if(argc > 2 && ((TString) argv[2]) == "ZEE") types = {"ZEE"};
+  if(argc > 2 && ((TString) argv[2]) == "ZMUMU") types = {"ZMUMU"};
   gROOT->SetBatch();
 
-  for(TString type : {"ZEE","ZMUMU"}){
+  std::ofstream out(tag + ".txt");
+  std::streambuf *coutbuf = std::cout.rdbuf(); //save old buf
+  std::cout.rdbuf(out.rdbuf()); //redirect std::cout to out.txt!
+
+  for(TString type : types){
     TFile *file = new TFile(getTreeLocation() + "outputs/rootfiles/" + type + "/" + tag + ".root");
     for(TString syst : {"","JESUp","JESDown","JERUp","JERDown","PUUp","PUDown","QGUp","ystarUp","mjjUp","mcfmUp"}){
       std::cout << std::endl << type << "\t\t" << syst << std::endl;
       std::map<TString, TH1D*> histos;
       std::map<TString, double> lumi;
       std::map<TString, double> expected;
-
+  
       lumi["data"] = 19788;
-      lumi["ZVBF"] = 835070;
+      lumi["ZVBF"] = 3.33264e+06;
       lumi["DY0"] = 8693.5; 
       lumi["DY1"] = 36087.7; 
       lumi["DY2"] = 101652; 
@@ -65,7 +72,7 @@ int main(){
       lumi["WW"] = 182363;
       lumi["WZ"] = 301123;
       lumi["ZZ"] = 555110;
-
+  
       histos["data"] 				= getPlot(file, "data", plot);
       histos["ZVBF"] 				= getPlot(file, "ZVBF", plot + syst);
       histos["DY"] 				= getPlot(file, "DY1", plot + syst);
@@ -75,21 +82,18 @@ int main(){
         safeAdd(histos["other"], getPlot(file, i, plot + syst, true));
       }
       histos["data"]->Add(histos["other"], -1);
-
+  
       for(auto hist = histos.begin(); hist != histos.end(); ++hist) expected[hist->first] = hist->second->Integral();
-
+  
       //Scale back to number of generated events for TFractionFitter (take DY1 for DY)
       histos["ZVBF"]->Scale(lumi["ZVBF"]/lumi["data"]);
       histos["DY"]->Scale(lumi["DY2"]/lumi["data"]);
-
+  
       std::vector<TString> parts {"ZVBF","DY"};
       TObjArray *mc = new TObjArray(parts.size());
       for(TString i : parts) mc->Add(histos[i]);
-
+  
       TFractionFitter* fit = new TFractionFitter(histos["data"], mc, "Q");
-//      fit->Constrain(1,0,0.02);
-//      fit->Constrain(2,0,1);
-//      fit->SetRangeX(16,22);
       if(fit->Fit() == 0){
         std::map<TString, double> fraction;
         std::map<TString, double> fractionError;
@@ -103,7 +107,7 @@ int main(){
           k[i] = fraction[i]/ratio[i];
           kError[i] = fractionError[i]/ratio[i];
         }
-
+  
         std::cout << "Chi²/NDF\t" << fit->GetChisquare()/fit->GetNDF() << std::endl << "\t\t\t";
         for(TString i : parts) std::cout << i << "\t\t";
         std::cout << std::endl << "fraction\t\t";
